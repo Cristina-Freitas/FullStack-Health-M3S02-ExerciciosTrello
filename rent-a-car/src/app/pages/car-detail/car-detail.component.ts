266a2d9974ec178b0cbf2484b172bfa4681169b2
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-11-10 22:35:58 -0300

    Ex 02 - Adaptação de Roteamento

--- rent-a-car/src/app/pages/car-detail/car-detail.component.ts
+++ rent-a-car/src/app/pages/car-detail/car-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CarService } from '../../services/car.service';
 
 @Component({
   selector: 'app-car-detail',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './car-detail.component.html',
   styleUrl: './car-detail.component.scss'
 })
-export class CarDetailComponent {
+export class CarDetailComponent implements OnInit {
+
+  car: any;
+  private carService = inject(CarService);
+  private route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    const carId = this.route.snapshot.paramMap.get('id');
+    if (carId) {
+      this.carService.getById(carId).subscribe({
+        next: (data) => (this.car = data),
+        error: (error) => console.error('Erro ao carregar detalhes do carro:', error)
+      });
+    }
+  }
 
 }
