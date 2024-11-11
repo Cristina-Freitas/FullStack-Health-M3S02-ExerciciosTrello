079335821ec16f0eda7efa645ea1c03ef1edd642
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-11-11 09:10:56 -0300

    Ex 06 - Funcionalidade de Favoritos

--- rent-a-car/src/app/pages/car-detail/car-detail.component.ts
+++ rent-a-car/src/app/pages/car-detail/car-detail.component.ts
@@ -1,19 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CarService } from '../../services/car.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-car-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './car-detail.component.html',
   styleUrl: './car-detail.component.scss'
 })
 export class CarDetailComponent implements OnInit {
 
   car: any;
+  isFavorite: boolean = false;
   private carService = inject(CarService);
   private route = inject(ActivatedRoute);
+  private storageKey: string = 'favorites';
+
 
   ngOnInit(): void {
     const carId = this.route.snapshot.paramMap.get('id');
@@ -25,4 +29,29 @@ export class CarDetailComponent implements OnInit {
     }
   }
 
+  // Verifica se o carro está nos favoritos
+  checkIfFavorite(id: string): boolean {
+    const favorites = JSON.parse(localStorage.getItem(this.storageKey) || '[]');
+    return favorites.some((favorite: any) => favorite.id === id);
+  }
+
+  // Adiciona ou remove o carro dos favoritos
+  toggleFavorite(): void {
+    let favorites = JSON.parse(localStorage.getItem(this.storageKey) || '[]');
+
+    if (this.isFavorite) {
+      // Remove dos favoritos
+      favorites = favorites.filter((favorite: any) => favorite.id !== this.car.id);
+      this.isFavorite = false;
+    } else {
+      // Adiciona aos favoritos
+      favorites.push(this.car);
+      this.isFavorite = true;
+    }
+
+    // Atualiza o LocalStorage
+    localStorage.setItem(this.storageKey, JSON.stringify(favorites));
+  }
 }
+
+
